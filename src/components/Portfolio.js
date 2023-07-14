de2269f6c29033c0f0ad7ea4e267e0e83d43bcaf
Ter C. Fiel <120294370+Terciel-22@users.noreply.github.com>
2023-07-14 13:16:12 +0800

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,9 +10,9 @@ const Portfolio = () => {
                     <div className="work">
                         <img src="https://i.pinimg.com/originals/29/5a/d5/295ad5526a6c566a415776b9dede64f9.jpg" alt="Admin dashboard" />
                         <div className="layer">
-                            <h3>E-commerce on Desktop</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum minus enim modi deserunt iste non.</p>
-                            <a href="#ecommerceondesktop">
+                            <h3>Countries</h3>
+                            <p>Displaying countries' information and data from REST API.</p>
+                            <a href="https://terciel-rest-countries-api.vercel.app/">
                                 <FontAwesomeIcon icon={faExternalLinkAlt}/>
                             </a>
                         </div>
